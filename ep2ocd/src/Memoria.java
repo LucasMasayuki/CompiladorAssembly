df6e471322fce690b97dc061fd3ea2d3bd9208d0
LucasMasayuki <lutamaribuchi@gmail.com>
2018-06-15 18:46:01 -0300

    .

--- ep2ocd/src/Memoria.java
+++ ep2ocd/src/Memoria.java
@@ -3,11 +3,11 @@ import java.util.LinkedList;
 class Memoria {
     private LinkedList filaDeProcesso = new LinkedList();
 
-    public void novoProcesso(String posicaoNaMemoria){
-        filaDeProcesso.addLast(posicaoNaMemoria);
+    public void novoProcesso(String palavra){
+        filaDeProcesso.addLast(palavra);
     }
 
-    public String pegaProcesso() {
-        return filaDeProcesso.removeLast();
+    public String pegaProcesso(int endereco) {
+        return (String)filaDeProcesso.get(endereco);
     }
 }
