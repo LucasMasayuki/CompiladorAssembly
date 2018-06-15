7f82cb616ee893b9530ea2bb256c0142f7f86ea6
LucasMasayuki <lutamaribuchi@gmail.com>
2018-06-15 18:19:43 -0300

    classe palavra

---
+++ ep2ocd/src/Palavra.java
@@ -0,0 +1,12 @@
+
+public class Palavra {
+	private String opcode;
+	private String operandoUm;
+	private String operandoDois;
+	
+	public Palavra(String opcode, String operandoUm, String operandoDois) {
+		this.opcode = opcode;
+		this.operandoUm = operandoUm;
+		this.operandoDois = operandoDois;
+	}
+}
