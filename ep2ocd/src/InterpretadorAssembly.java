91cc4046010a6ae46272dfead1ac301c47f2835b
Lucas <Lucas@DESKTOP-4NUCQGA>
2018-06-21 01:46:54 -0300

    interface grafica inicio

--- ep2ocd/src/InterpretadorAssembly.java
+++ ep2ocd/src/InterpretadorAssembly.java
@@ -1,56 +1,7 @@
-import java.util.*;
 class InterpretadorAssembly {
 
-    public static void main(String[] args) {
-
-        Scanner entrada = new Scanner(System.in);
-        int opcao = 0;
-
-        while (opcao != 2) {
-
-            System.out.println("Bem vindo ao compilador assembly 2018");
-            System.out.println("1 - iniciar");
-            System.out.println("2 - Sair ");
-            opcao = entrada.nextInt();
-
-            switch (opcao) {
-
-                case 1:
-                    int continuar = 1;
-                    Scanner input = new Scanner(System.in);
-                    while (continuar == 1) {
-                        System.out.println("");
-                        System.out.println("Coloque o comando em assembly que vc desejar");
-                        String instrucao = input.nextLine();
-                        compila(instrucao);
-                        System.out.println("");
-                        System.out.println("Deseja colocar mais um comando ?");
-                        System.out.println("1 - Sim");
-                        System.out.println("2 - Não");
-                        continuar = entrada.nextInt();
-                    }
-
-                    break;
-
-                case 2:
-
-                    System.out.println("Finalizando o programa");
-                    break;
-
-                default:
-
-                    System.out.println("Este nao e uma opcao valida");
-                    break;
-            }
-            
-        }
-    }
-
-    public static void compila(String entrada) {
+    public static void compila(String entrada, Memoria memoria, Uc uc, Comandos comandosAssembly) {
         Palavra palavra;
-        Uc uc = new Uc();
-        Memoria memoria = new Memoria();
-        Comandos comandosAssembly = new Comandos();
         StringBuilder instrucao = new StringBuilder();
         StringBuilder comando = new StringBuilder(entrada);
         String opcode;
